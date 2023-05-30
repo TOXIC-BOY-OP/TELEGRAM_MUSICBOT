af7e1a8ca2e652a00b3bf2f8ce629f650cbf8f9d
TOXIC-BOY-OP <134930342+TOXIC-BOY-OP@users.noreply.github.com>
2023-05-30 14:25:44 +0530

    config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ load_dotenv()
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+BOT_NAME = getenv("𝐓𝐎𝐗𝐈𝐂 𝐌𝐔𝐒𝐈𝐂 𝐎𝐏")
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("25374274"))
+API_HASH = getenv("d0012f0876e6f9308eec2f474e7b9273")
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("6109531260").split()))
